2f441c9b7f810c07536efb5415a3a998599d691f
lijunjie2232 <2533584225@qq.com>
2025-12-21 18:17:17 +0900

    init commit

---
+++ moji_spider/__init__.py
@@ -0,0 +1,9 @@
+from .configs import __HEADERS__, __CONFIG__, __TORTOISE_ORM__
+from . import schemas
+
+__ALL__ = [
+    "__HEADERS__",
+    "__CONFIG__",
+    "__TORTOISE_ORM__",
+    "schemas",
+]
