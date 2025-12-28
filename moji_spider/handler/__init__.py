3f249bb489f0976178500aacc0ee97957a829d63
lijunjie2232 <2533584225@qq.com>
2025-12-28 11:52:13 +0900

    add executors

--- moji_spider/handler/__init__.py
+++ moji_spider/handler/__init__.py
@@ -1,6 +1,10 @@
+from .content import EXECUTOR, ITEM_EXECUTOR, TASK_EXECUTOR
 from .folder import offical_folder_to_db, share_folder_to_db
 
 __ALL__ = [
     "share_folder_to_db",
     "offical_folder_to_db",
+    "ITEM_EXECUTOR",
+    "TASK_EXECUTOR",
+    "EXECUTOR",
 ]
