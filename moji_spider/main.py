48cbb559a6998b6669ab7e23594b3335f569a10d
lijunjie2232 <2533584225@qq.com>
2026-01-08 22:29:44 +0900

    spider code

--- moji_spider/main.py
+++ moji_spider/main.py
@@ -14,15 +14,21 @@ from .handler import (
 
 __LOOP__ = asyncio.get_event_loop()
 
-parrel_num = 7
+parrel_num = 10
 
 
 async def main():
     """
     main entry
     """
+    # await offical_folder_to_db()
+    # await share_folder_to_db()
+
+    # exit(0)
+
     # Run the async function
     task = asyncio.create_task(EXECUTOR())
-    task_t = [asyncio.create_task(TASK_EXECUTOR()) for _ in range(parrel_num)]
-    task_i = [asyncio.create_task(ITEM_EXECUTOR()) for _ in range(parrel_num)]
+    task_t = [asyncio.create_task(TASK_EXECUTOR(id)) for id in range(parrel_num)]
+    # task_i = [asyncio.create_task(ITEM_EXECUTOR(id)) for id in range(parrel_num)]
+    task_i = [asyncio.create_task(ITEM_EXECUTOR())]
     await asyncio.gather(task, *task_t, *task_i)
